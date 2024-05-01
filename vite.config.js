3c952ee1fa1361709ae50d2ff6f707f6c8f7f2b7
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2024-05-01 14:26:23 +0800

    Allow top-level await in Vite build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { defineConfig } from 'vite';
+import topLevelAwait from 'vite-plugin-top-level-await';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-    base: "",
-    plugins: [svelte()],
+  base: '',
+  plugins: [
+    svelte(),
+    topLevelAwait({
+      promiseExportName: '__tla',
+      promiseImportName: i => `__tla_${i}`,
+    }),
+  ],
 });
